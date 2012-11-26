fc59cf43cf0e6636fd5244a3ef1fffbdb7098c27
John Wiegley <johnw@newartisans.com>
2012-11-26 15:32:14 -0600

    Remove data submodule

---
+++ data/70/E63F8E-8561-41F9-A1F1-48AB75A435AA/getquote-uk.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import urllib, string, sys
+
+def download(sym):
+    url     = "http://uk.finance.yahoo.com/d/quotes.csv?s="
+    #url  = "http://finance.yahoo.com/d/quotes.csv?s="
+    url += sym + "&f=sl1d1t1c1ohgv&e=.csv"
+    f = urllib.urlopen(url, proxies={})
+    info = f.read()
+    f.close()
+    fields = string.split(info, ',')
+    result = float(fields[1])/100
+    return result
+
+
+sym = sys.argv[1]
+sym = sym.replace('_', '.')
+if sym == '£':
+	print '£1.00'
+else:
+	try:
+    		print "£" +str(download(sym))
+	except:
+    		pass
